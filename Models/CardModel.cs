faf60720703b68a8769cd03b55e5899f359915f8
xznosage2 <gzellner@student.neumont.edu>
2024-02-29 17:37:27 -0700

    addedModels

---
+++ Models/CardModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoForms.Models
+{
+    internal class CardModel
+    {
+        public int id;
+
+        public string suit;
+
+        public int number;
+
+        public int value;
+    }
+}
---
+++ Models/PlayerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoForms.Models
+{
+    internal class PlayerModel
+    {
+        public int id;
+
+        public string name;
+
+        public int? money;
+
+        public List<CardModel> hand;
+
+        public List<CardModel>? splitHand;
+
+
+
+    }
+}
