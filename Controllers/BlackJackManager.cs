127996eb6fc8f9f3841e427b0a6b0fbdb2b7a349
gotagetfood <96447537+gotagetfood@users.noreply.github.com>
2024-02-29 19:02:56 -0700

    blackjack manager

--- Controllers/BlackJackManager.cs
+++ Controllers/BlackJackManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasinoForms.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,38 @@ namespace CasinoForms.Controllers
 {
     internal class BlackJackManager
     {
+        PlayerModel GetScore(PlayerModel person)
+        {
+            bool acedUp = false;
+            int total = 0;
+            foreach (var card in person.hand)
+            {
+                total += card.value;
+                if (card.value == 1) acedUp = true;
+            }
+            if (acedUp && total + 10 <= 21)
+            {
+                total += 10;
+            }
+            person.hand1Value = total;
+
+            if (person.splitHand != null)
+            {
+                total = 0;
+                acedUp = false;
+                foreach (var card in person.splitHand)
+                {
+                    total += card.value;
+                    if (card.value == 1) acedUp = true;
+                }
+                if (acedUp && total + 10 <= 21)
+                {
+                    total += 10;
+                }
+                person.hand2Value = total;
+            }
+            return person;
+        }
+
     }
 }
